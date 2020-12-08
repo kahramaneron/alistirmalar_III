c24cf809a3d4d558907c8cc038dcb4e4ff16c3e0
Erol Kahraman <69480343+kahramaneron@users.noreply.github.com>
2020-12-08 16:36:29 +0300

    Update soru_7.py

--- soru_7.py
+++ soru_7.py
@@ -4,19 +4,15 @@ def bubleSort(dizi):
 
     n = len(dizi)
 
-    for i in range(0,n-1): 
+    for i in range(0,n-1): #dizi içerisinde n-1. indekse kadar gezecek
 
         for j in range(0,n-i-1):
 
-            if dizi[j]> dizi[j+1]:
-
+            if dizi[j]> dizi[j+1]:  #dizide j.değer kendisinden bir sonraki değerden büyükse yer değiştirecek ve büyük olan değer adım adım dizinin sağına doğru kayacak.
+                                    #böylelikle her adımda dizinin elemanlarının sırası belli olmaya başlar.
                 dizi[j],dizi[j+1] = dizi[j+1],dizi[j]
 
-            j +=1
-
-
     return dizi
-
 
 print("Siralanmis Dizi: "+ str(bubleSort([33,1,0,-2,2,4,3])))
 
